fd8efdd09275910cde189caec8db1c7a2e44bc56
unknown <Administrateur@42SE34-2H0455J.ad.campus-eni.fr>
2020-10-06 15:10:38 +0200

    tp1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,43 @@ import { Component } from '@angular/core';
 export class HomePage {
   public pseudo : string ='';
   public difficulty : string = '';
+  // public memorisation = [];
+  public error : string = "";
+  public checkbox : boolean = false;
+  public hidden : boolean = true;
+  public hiddenBtn : boolean = false;
+  
+
   constructor() {}
+
+  public start(){
+    this.error = "";
+    
+    if (this.pseudo.length < 3) {
+      this.error = "Veuillez rentrer un pseudo d'au moins 3 caractères.";
+      return;
+    }
+    if (this.difficulty === undefined) {
+      this.error = "Veuillez rentrer une difficulté.";
+      return;
+    }
+
+    this.hidden = false;
+        
+
+    // if (this.checkbox) {
+    //   this.memorisation = [
+    //     {
+    //       PseudoM : this.pseudo,
+    //       difficulty : this.difficulty
+    //     }
+    //   ];
+    // }
+
+  }
+
+  public answer(){
+    this.hiddenBtn = !this.hiddenBtn;
+  }
 
 }
