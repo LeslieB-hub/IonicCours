f7f117e75ee3bce7f3ecb340dddc80dedff1fb24
unknown <Administrateur@42SE34-2H0455J.ad.campus-eni.fr>
2020-10-06 12:41:09 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public pseudo : string ='';
+  public difficulty : string = '';
   constructor() {}
 
 }
