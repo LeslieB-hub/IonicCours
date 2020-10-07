89d8e4adef35ca148a4833b88526c2803fe0a346
unknown <Administrateur@42SE34-2H0455J.ad.campus-eni.fr>
2020-10-07 17:00:02 +0200

    tp3 not finished

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController, ToastController } from '@ionic/angular';
+import { Question } from '../Models/question';
+import { OpenTriviaProvider } from '../Provider/OpenTriviaService.provider';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +9,23 @@ import { AlertController, ToastController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  public pseudo : string ='';
+  public pseudo : string ='Test';
   public difficulty : string = 'easy';
   // public memorisation = [];
   public error : string = "";
   public checkbox : boolean = false;
-  public hidden : boolean = true;
-  public hiddenBtn : boolean = false;
+  public beginGame : boolean = false;
+  public nextQuestion : boolean=  false;
+  public questions = [];
+  public questionDisplay : Question;
+  public indexQuestion : number = 0;
+  public answers = [];
+  public points : number = 0;
+  public colorBtn : string = "primary"; 
   
 
-  constructor(private alertCtrl: AlertController, public toastCtrl: ToastController)  {}
+  constructor(private alertCtrl: AlertController, public toastCtrl: ToastController,
+              private trivialService : OpenTriviaProvider)  {}
 
   public async start(){
     this.error = "";
@@ -42,7 +51,7 @@ export class HomePage {
       return;
     }
 
-    this.hidden = false;
+    this.beginGame = true;
         
 
     if (this.checkbox) {
@@ -55,10 +64,63 @@ export class HomePage {
       // ];
     }
 
+    //appel pr récup tableau de questions
+    await this.getTableQuestion();
+    
+    this.getQuestionDisplay();
+    
+    
+  }
+
+
+
+  public getTableQuestion(){
+    this.trivialService.getQuestions(2, this.difficulty)
+    .then((results)=> {
+      this.questions = results;
+    })
+    .catch(async (err)=> {
+      const alert = await this.alertCtrl.create({
+        header : 'Erreur appele Service',
+        message : "Impossible de récupérer les films.",
+        buttons : ["OK"]
+      });
+    });
+  }
+
+
+
+
+  public getQuestionDisplay(){
+    console.log(this.questions[this.indexQuestion]);
+    if (this.indexQuestion<this.questions.length) {
+      //Récupérer la question ds l'ordre
+      this.questionDisplay = this.questions[this.indexQuestion];
+      //incrémenter l'index des question
+      this.indexQuestion++;
+      //mettre ttes les réponses dans l'array answer et mélanger
+      this.answers = this.questionDisplay.incorrect_answers;
+      this.answers.push(this.questionDisplay.correct_answer);
+      console.log(this.answers);
+      this.answers.sort(function () { return 0.5 - Math.random() });
+      console.log(this.answers);
+            
+    }else{
+      this.indexQuestion = 0;
+    }
+    
+   
   }
 
   public answer(reponse : string){
-    this.hiddenBtn = !this.hiddenBtn;
+    this.nextQuestion = true;
+    if (reponse == this.questionDisplay.correct_answer) {
+      console.log(reponse);
+      this.points++;
+      this.colorBtn = "danger";
+      
+    }
+        
   }
 
 }
