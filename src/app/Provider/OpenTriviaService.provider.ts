2153f01b39e9330be98c9610353643265494ea00
unknown <Administrateur@42SE34-2H0455J.ad.campus-eni.fr>
2020-10-08 14:51:27 +0200

    tp7 finished

--- src/app/Provider/OpenTriviaService.provider.ts
+++ src/app/Provider/OpenTriviaService.provider.ts
@@ -1,42 +1,65 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { promise } from 'protractor';
 import { Question } from '../Models/question';
 
-@Injectable()
+@Injectable({
+    providedIn:'root' //permet de le déclarer ici au lieu de ds app.module.ts
+})
 export class OpenTriviaProvider{
-    public getQuestions(nbQuestions : number, difficulty : string)
-    :Promise<Array<Question>>{
+
+    constructor(private httpClient : HttpClient){}
+
+    public getQuestions(nbQuestions : number, difficulty : string):Promise<Array<Question>>{
         return new Promise((resolve, reject) => {
-            resolve([
-                { category: "Entertainment: Japanese Anime & Manga",
-                type: "multiple",
-                difficulty: "easy",
-                question: "In &quot;Fairy Tail&quot;, what is the nickname of Natsu Dragneel?",
-                correct_answer: "The Salamander",
-                incorrect_answers: ["The Dragon Slayer", "The Dragon", "The Demon"]
-                },
-                { category: "Entertainment: Video Games",
-                type: "boolean",
-                difficulty: "medium",
-                question: "&quot;Return to Castle Wolfenstein&quot; was the only game of the Wolfenstein series where you don&#039;t play as William &quot;B.J.&quot; Blazkowicz.",
-                correct_answer: "False",
-                incorrect_answers: ["True"]
-                },
-                { category: "Entertainment: Japanese Anime & Manga",
-                type: "multiple",
-                difficulty: "easy",
-                question: "In &quot;Fairy Tail&quot;, what is the nickname of Natsu Dragneel?",
-                correct_answer: "The Salamander",
-                incorrect_answers: ["The Dragon Slayer", "The Dragon", "The Demon"]
-                },
-                { category: "Entertainment: Video Games",
-                type: "boolean",
-                difficulty: "medium",
-                question: "&quot;Return to Castle Wolfenstein&quot; was the only game of the Wolfenstein series where you don&#039;t play as William &quot;B.J.&quot; Blazkowicz.",
-                correct_answer: "False",
-                incorrect_answers: ["True"]
-                }
-            ]);
+            let params = new HttpParams();
+            params = params.append('amount', String(nbQuestions));
+            params = params.append('difficulty', difficulty);
+            this.httpClient.get('https://opentdb.com/api.php', {params:params})
+            .toPromise()
+            .then((response)=>{
+                if ( response['response_code']== 0) {
+                    resolve(response['results']);
+                }else{
+                    reject("Le serveur n'a pas retourné de valeur!!");
+                } 
+            })
+            .catch((error)=>{
+                reject(error);
+            });
+
+
         })
     }
+
+
+                // resolve([
+            //     { category: "Entertainment: Japanese Anime & Manga",
+            //     type: "multiple",
+            //     difficulty: "easy",
+            //     question: "In &quot;Fairy Tail&quot;, what is the nickname of Natsu Dragneel?",
+            //     correct_answer: "The Salamander",
+            //     incorrect_answers: ["The Dragon Slayer", "The Dragon", "The Demon"]
+            //     },
+            //     { category: "Entertainment: Video Games",
+            //     type: "boolean",
+            //     difficulty: "medium",
+            //     question: "&quot;Return to Castle Wolfenstein&quot; was the only game of the Wolfenstein series where you don&#039;t play as William &quot;B.J.&quot; Blazkowicz.",
+            //     correct_answer: "False",
+            //     incorrect_answers: ["True"]
+            //     },
+            //     { category: "Entertainment: Japanese Anime & Manga",
+            //     type: "multiple",
+            //     difficulty: "easy",
+            //     question: "In &quot;Fairy Tail&quot;, what is the nickname of Natsu Dragneel?",
+            //     correct_answer: "The Salamander",
+            //     incorrect_answers: ["The Dragon Slayer", "The Dragon", "The Demon"]
+            //     },
+            //     { category: "Entertainment: Video Games",
+            //     type: "boolean",
+            //     difficulty: "medium",
+            //     question: "&quot;Return to Castle Wolfenstein&quot; was the only game of the Wolfenstein series where you don&#039;t play as William &quot;B.J.&quot; Blazkowicz.",
+            //     correct_answer: "False",
+            //     incorrect_answers: ["True"]
+            //     }
+            // ]);
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,9 +19,10 @@ export class HomePage {
   public questions = [];
   public questionDisplay : Question;
   public indexQuestion : number = 0;
-  public answers = [];
+  //public answers = [];
   public points : number = 0;
   public colorBtn : string = "primary"; 
+  public endGame : boolean = false;
   
 
   constructor(private alertCtrl: AlertController, public toastCtrl: ToastController,
@@ -51,9 +52,14 @@ export class HomePage {
       return;
     }
 
+    
+    //appel pr récup tableau de questions
+    await this.getTableQuestion();
+    
+    this.getQuestionDisplay();
+    
     this.beginGame = true;
         
-
     if (this.checkbox) {
       this.error = "Sauvegarde";
       // this.memorisation = [
@@ -63,64 +69,107 @@ export class HomePage {
       //   }
       // ];
     }
-
-    //appel pr récup tableau de questions
-    await this.getTableQuestion();
-    
-    this.getQuestionDisplay();
-    
     
   }
 
 
 
-  public getTableQuestion(){
-    this.trivialService.getQuestions(2, this.difficulty)
-    .then((results)=> {
-      this.questions = results;
-    })
-    .catch(async (err)=> {
+  public async getTableQuestion(){
+    try {
+      this.questions = await this.trivialService.getQuestions(2, this.difficulty);
+      
+    } catch (error) {
       const alert = await this.alertCtrl.create({
         header : 'Erreur appele Service',
         message : "Impossible de récupérer les films.",
         buttons : ["OK"]
       });
-    });
+    }
+
   }
 
 
 
+
+  // public getQuestionDisplay(){
+  //   console.log(this.questions[this.indexQuestion]);
+  //   if (this.indexQuestion<this.questions.length) {
+  //     //Récupérer la question ds l'ordre
+  //     this.questionDisplay = this.questions[this.indexQuestion];
+  //     //incrémenter l'index des question
+  //     this.indexQuestion++;
+  //     //mettre ttes les réponses dans l'array answer et mélanger
+  //     this.answers = this.questionDisplay.incorrect_answers;
+  //     this.answers.push(this.questionDisplay.correct_answer);
+  //     console.log(this.answers);
+  //     this.answers.sort(function () { return 0.5 - Math.random() });
+  //     console.log(this.answers);
+            
+  //   }else{
+  //     this.indexQuestion = 0;
+  //   }       
+  // }
 
   public getQuestionDisplay(){
-    console.log(this.questions[this.indexQuestion]);
-    if (this.indexQuestion<this.questions.length) {
-      //Récupérer la question ds l'ordre
-      this.questionDisplay = this.questions[this.indexQuestion];
-      //incrémenter l'index des question
-      this.indexQuestion++;
-      //mettre ttes les réponses dans l'array answer et mélanger
-      this.answers = this.questionDisplay.incorrect_answers;
-      this.answers.push(this.questionDisplay.correct_answer);
-      console.log(this.answers);
-      this.answers.sort(function () { return 0.5 - Math.random() });
-      console.log(this.answers);
-            
-    }else{
-      this.indexQuestion = 0;
-    }
-    
-   
-  }
-
-  public answer(reponse : string){
-    this.nextQuestion = true;
-    if (reponse == this.questionDisplay.correct_answer) {
-      console.log(reponse);
-      this.points++;
-      this.colorBtn = "danger";
+    this.questionDisplay = this.questions[this.indexQuestion];
+    //JS permet de rajouter des attributs dynamiques à un objet courant. avec les []
+    //au lieu de mettre des attributs optionnels ds la class Question
+    this.questionDisplay["reponses"] = [];
+    for (let reponse of this.questionDisplay["incorrect_answers"]) {
+      //TS permet d'ajouter un objet js avec {} objet sans nom
+       this.questionDisplay["reponses"].push({
+        answer : reponse,
+        correct : false
+      });
       
     }
-        
+    this.questionDisplay["reponses"].push({
+      answer : this.questionDisplay["correct_answer"],
+      correct : true
+    });
+    this.questionDisplay["reponses"] = this.shuffleArray(this.questionDisplay["reponses"]);
   }
+
+  public shuffleArray(array){
+    for (var i = array.length - 1; i > 0; i--)
+     { var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i]; array[i] = array[j];
+      array[j] = temp;}
+      return array;
+      }
+  
+  public answer(reponse : string){
+    this.nextQuestion = true;
+    if (reponse["correct"]) {
+      this.points++;     
+    }
+
+    if (this.indexQuestion >= this.questions.length -1) {
+      this.endGame =true;
+    }
+  }
+
+  public questionNext(){
+    if (this.indexQuestion < this.questions.length -1) {
+      this.indexQuestion++;
+      this.nextQuestion = false;
+      this.getQuestionDisplay();
+    }
+  }
+
+  public back(){
+    this.beginGame = false;
+  }
+
+  // public answer(reponse : string){
+  //   this.nextQuestion = true;
+  //   if (reponse == this.questionDisplay.correct_answer) {
+  //     console.log(reponse);
+  //     this.points++;
+  //     this.colorBtn = "danger";
+      
+  //   }
+        
+  // }
 
 }
